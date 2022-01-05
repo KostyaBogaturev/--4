0b43d4fbb638e6fc9a9c2a5f25d9f19676c74fb1
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2022-01-05 11:27:11 +0200

    lab4

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php echo "<form id='forma' action='test.php' method='post'> 
+    <h1>Форма входа</h1> 
+    <p>Заполните поля для входа на сайт</p> 
+    <p>Логин<br /><input type='text' name='login'></p> 
+    <p>Пароль<br /><input type='password' name='password'></p> 
+    <p><input type='submit' name='submit' value='Войти'> <br></p></form>" ?>
+</body>
